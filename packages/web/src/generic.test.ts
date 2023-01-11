1d99f66f409bc7cf37e664f3e77e4c2db35ef9f8
Tobias Lins <me@tobi.sh>
2023-01-11 08:01:23 +0100

    Check if track function successfully queues events into vaq

--- packages/web/src/generic.test.ts
+++ packages/web/src/generic.test.ts
@@ -1,4 +1,4 @@
-import { inject } from './generic';
+import { inject, track } from './generic';
 
 describe('inject', () => {
   describe('in development mode', () => {
@@ -36,6 +36,85 @@ describe('inject', () => {
 
       expect(script.src).toEqual('http://localhost/_vercel/insights/script.js');
       expect(script).toHaveAttribute('defer');
+    });
+  });
+});
+
+describe('track  custom events', () => {
+  beforeEach(() => {
+    // reset the internal queue before every test
+    window.vaq = [];
+  });
+  describe('queue custom events', () => {
+    it('event name only', () => {
+      inject();
+
+      track('my event');
+
+      expect(window.vaq).toBeDefined();
+
+      if (!window.vaq) throw new Error('window.vaq is not defined');
+
+      expect(window.vaq[0]).toEqual([
+        'track',
+        {
+          name: 'my event',
+        },
+      ]);
+    });
+    it('with custom data', () => {
+      inject();
+
+      track('custom event', {
+        string: 'string',
+        number: 1,
+      });
+
+      expect(window.vaq).toBeDefined();
+
+      if (!window.vaq) throw new Error('window.vaq is not defined');
+
+      expect(window.vaq[0]).toEqual([
+        'track',
+        {
+          name: 'custom event',
+          data: {
+            string: 'string',
+            number: 1,
+          },
+        },
+      ]);
+    });
+
+    it('strip nested object', () => {
+      process.env = {
+        NODE_ENV: 'production',
+      };
+      inject();
+
+      track('custom event', {
+        string: 'string',
+        number: 1,
+        // @ts-expect-error ignore for test
+        nested: {
+          object: '',
+        },
+      });
+
+      expect(window.vaq).toBeDefined();
+
+      if (!window.vaq) throw new Error('window.vaq is not defined');
+
+      expect(window.vaq[0]).toEqual([
+        'track',
+        {
+          name: 'custom event',
+          data: {
+            string: 'string',
+            number: 1,
+          },
+        },
+      ]);
     });
   });
 });
