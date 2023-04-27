fec83c9c8106108f3aa53b922854250e563b212b
Timo Lins <me@timo.sh>
2023-04-27 15:32:42 +0200

    Remove doc type from prop object

--- packages/web/src/types.ts
+++ packages/web/src/types.ts
@@ -14,15 +14,6 @@ export type AllowedPropertyValues = string | number | boolean | null;
 
 export type BeforeSend = (event: BeforeSendEvent) => BeforeSendEvent | null;
 
-/**
- * Analytics properties.
- * @param [props.mode] - The mode to use for the analytics script. Defaults to `auto`.
- * - `auto` - Automatically detect the environment.  Uses `production` if the environment cannot be detected.
- * - `production` - Always use the production script. (Sends events to the server)
- * - `development` - Always use the development script. (Logs events to the console)
- * @param [props.debug] - Whether to enable debug logging in development. Defaults to `true`.
- * @param [props.beforeSend] - A middleware function to modify events before they are sent. Should return the event object or `null` to cancel the event.
- */
 export interface AnalyticsProps {
   beforeSend?: BeforeSend;
   debug?: boolean;
