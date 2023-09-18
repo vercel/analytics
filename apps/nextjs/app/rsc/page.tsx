70d266a09c84bbd463335bed725791ed65620513
Tobias Lins <me@tobi.sh>
2023-09-18 14:00:37 +0200

    update next

--- apps/nextjs/app/rsc/page.tsx
+++ apps/nextjs/app/rsc/page.tsx
@@ -1,6 +1,8 @@
+import { cookies } from 'next/headers';
 import { track } from '@vercel/analytics/server';
 
 export default async function RSC() {
+  cookies();
   track('Viewed Experiment');
 
   return <div>I did track a server action on render</div>;
