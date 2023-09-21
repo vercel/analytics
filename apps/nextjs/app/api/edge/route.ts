490989974a4d6cd8090e9175dbb948a4cc5a31a4
Tobias Lins <me@tobi.sh>
2023-09-21 10:47:19 +0200

    Fix example

--- apps/nextjs/app/api/edge/route.ts
+++ apps/nextjs/app/api/edge/route.ts
@@ -3,7 +3,7 @@ import { track } from '@vercel/analytics/server';
 export const runtime = 'edge';
 
 export const GET = async function handler() {
-  track('Edge Event', {
+  await track('Edge Event', {
     data: 'edge',
     router: 'app',
   });
