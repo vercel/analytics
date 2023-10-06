d292a94e07c2d992062fa99b6353fa9337543516
Tobias Lins <me@tobi.sh>
2023-10-06 12:52:27 +0200

    Fix tests

--- packages/web/jest.setup.ts
+++ packages/web/jest.setup.ts
@@ -1,8 +1,5 @@
 import '@testing-library/jest-dom';
 
-// Adds helpers like `.toHaveAttribute`
-import '@testing-library/jest-dom/extend-expect';
-
 beforeEach(() => {
   // reset dom before each test
   document.getElementsByTagName('html')[0].innerHTML = '';
