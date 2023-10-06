b31b6e7aedb996605e38b5e23b63aa144a05c05f
Tobias Lins <me@tobi.sh>
2023-10-06 08:08:13 +0200

    Update middleware example & nextjs

--- apps/nextjs/middleware.ts
+++ apps/nextjs/middleware.ts
@@ -2,19 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextFetchEvent, NextRequest } from 'next/server';
 import { track } from '@vercel/analytics/server';
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
   event.waitUntil(
-    track(
-      'Redirect',
-      {
-        path: request.nextUrl.pathname,
-        type: 'waitUntil',
-      },
-      {
-        request,
-      },
-    ),
+    track('Redirect', {
+      path: request.nextUrl.pathname,
+      type: 'waitUntil',
+    }),
   );
   return NextResponse.redirect(new URL('/server-actions', request.url));
 }
