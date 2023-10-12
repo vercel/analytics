e38e9e3837850b984037bdc7ea297f5b1633d625
Tobias Lins <me@tobi.sh>
2023-10-12 08:31:10 +0200

    Remove log

--- apps/nextjs/app/errors/error.tsx
+++ apps/nextjs/app/errors/error.tsx
@@ -4,7 +4,6 @@ import { track } from '@vercel/analytics';
 
 export default function Error({ error }: { error: Error; reset: () => void }) {
   useEffect(() => {
-    console.log('throwing error');
     track('thrown-error', {
       message: error.message,
       time: new Date().toLocaleString(),
