2a1a6aa3b73090a2d1acb2b6d843962bd5f7e0f0
Tobias Lins <me@tobi.sh>
2024-01-31 11:46:11 +0100

    Add tests

--- packages/web/src/utils.test.ts
+++ packages/web/src/utils.test.ts
@@ -1,4 +1,4 @@
-import { getMode, parseProperties, setMode } from './utils';
+import { computeRoute, getMode, parseProperties, setMode } from './utils';
 
 describe('utils', () => {
   describe('parse properties', () => {
@@ -96,6 +96,101 @@ describe('utils', () => {
       it('should overwrite when set manually', () => {
         setMode('production');
         expect(getMode()).toBe('production');
+      });
+    });
+  });
+
+  describe('computeRoute()', () => {
+    it('returns unchanged pathname if no pathParams provided', () => {
+      expect(computeRoute('/vercel/next-site/analytics', null)).toBe(
+        '/vercel/next-site/analytics'
+      );
+    });
+
+    it('returns null for null pathname', () => {
+      expect(computeRoute(null, {})).toBe(null);
+    });
+
+    it('replaces segments', () => {
+      const input = '/vercel/next-site/analytics';
+      const params = {
+        teamSlug: 'vercel',
+        project: 'next-site',
+      };
+      const expected = '/[teamSlug]/[project]/analytics';
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    it('replaces segments even one param is not used', () => {
+      const input = '/vercel/next-site/analytics';
+      const params = {
+        lang: 'en',
+        teamSlug: 'vercel',
+        project: 'next-site',
+      };
+      const expected = '/[teamSlug]/[project]/analytics';
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    it('must not replace partial segments', () => {
+      const input = '/next-site/vercel-site';
+      const params = {
+        teamSlug: 'vercel',
+      };
+      const expected = '/next-site/vercel-site'; // remains unchanged because "vercel" is a partial match
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    it('handles array segments', () => {
+      const input = '/en/us/next-site';
+      const params = {
+        langs: ['en', 'us'],
+        teamSlug: 'vercel',
+      };
+      const expected = '/[...langs]/next-site';
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    it('handles special characters in url', () => {
+      const input = '/123/test(test';
+      const params = {
+        teamSlug: '123',
+        project: 'test(test',
+      };
+
+      const expected = '/[teamSlug]/[project]';
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    it('handles special more characters', () => {
+      const input = '/123/tes\\t(test/3.*';
+      const params = {
+        teamSlug: '123',
+      };
+
+      const expected = '/[teamSlug]/tes\\t(test/3.*';
+      expect(computeRoute(input, params)).toBe(expected);
+    });
+
+    describe('edge case handling (same values for multiple params)', () => {
+      it('replaces based on the priority of the pathParams keys', () => {
+        const input = '/test/test';
+        const params = {
+          teamSlug: 'test',
+          project: 'test',
+        };
+        const expected = '/[teamSlug]/[project]'; // 'teamSlug' takes priority over 'project' based on their order in the params object
+        expect(computeRoute(input, params)).toBe(expected);
+      });
+
+      it('handles reversed priority', () => {
+        const input = '/test/test';
+        const params = {
+          project: 'test',
+          teamSlug: 'test',
+        };
+        const expected = '/[project]/[teamSlug]'; // 'project' takes priority over 'teamSlug' here due to the reversed order in the params object
+        expect(computeRoute(input, params)).toBe(expected);
       });
     });
   });
