80e6c340f80e274281a7b0b5ea326d55f87e228e
Eric Spishak-Thomas <eric@vercel.com>
2024-11-08 10:10:45 -0500

    prefiv environment variable with next_public_ so it gets inlined at build time

--- packages/web/src/generic.ts
+++ packages/web/src/generic.ts
@@ -66,7 +66,7 @@ function inject(
   if (props.endpoint) {
     script.dataset.endpoint = props.endpoint;
   } else if (process.env.ENDPOINT_PATH_PREFIX) {
-    script.dataset.endpoint = `/${process.env.ENDPOINT_PATH_PREFIX}/_vercel/insights`;
+    script.dataset.endpoint = `/${process.env.NEXT_PUBLIC_ENDPOINT_PATH_PREFIX}/_vercel/insights`;
   }
   if (props.dsn) {
     script.dataset.dsn = props.dsn;
