2256fc6b0024c98e3d12175a416efbbec65c90d6
Hugo Richard <hugo.richard@vercel.com>
2025-11-18 12:05:25 +0000

    refactor(nuxt): update module import and enhance plugin setup

--- packages/web/src/nuxt/index.ts
+++ packages/web/src/nuxt/index.ts
@@ -1,40 +1,54 @@
-import { defineNuxtModule } from '@nuxt/kit';
+/* eslint-disable -- Nuxt module file */
+import { defineNuxtModule, addPlugin, addTemplate } from '@nuxt/kit';
 import type { NuxtModule } from '@nuxt/schema';
-import { useRoute } from 'vue-router';
-import { inject, pageview } from '../generic';
-// import type { AnalyticsProps, BeforeSend, BeforeSendEvent } from '../types';
-// import { createComponent } from '../vue/create-component';
+import type { AnalyticsProps, BeforeSend, BeforeSendEvent } from '../types';
+import { createComponent } from '../vue/create-component';
 
-// export const Analytics = createComponent('nuxt');
-// export type { AnalyticsProps, BeforeSend, BeforeSendEvent };
+export const Analytics = createComponent('nuxt');
+export type { AnalyticsProps, BeforeSend, BeforeSendEvent };
 
 export default defineNuxtModule({
   meta: {
     name: '@vercel/analytics',
     configKey: 'analytics',
+    docs: 'https://vercel.com/docs/analytics/quickstart',
   },
-  setup(options, nuxt) {
-    nuxt.options.plugins.push(() => {
-      console.log('Plugin loaded');
-      const route = useRoute();
+  setup() {
+    const template = addTemplate({
+      filename: 'vercel-analytics.client.ts',
+      getContents: () => `
+import { inject, pageview } from '@vercel/analytics'
+import { defineNuxtPlugin, useRoute, useNuxtApp, onNuxtReady } from '#imports'
 
-      nuxt.hook('app:ready', () => {
-        console.log('App ready');
-        inject({
-          disableAutoTrack: true,
-          framework: 'nuxt',
-        });
-        pageview({
-          route: route.matched[0]?.path || route.path,
-          path: route.path,
-        });
-      });
-      nuxt.hooks.hook('page:finish', () => {
-        pageview({
-          route: route.matched[0]?.path || route.path,
-          path: route.path,
-        });
-      });
+export default defineNuxtPlugin(() => {
+  const nuxtApp = useNuxtApp()
+  const route = useRoute()
+
+  onNuxtReady(() => {
+    inject({
+      disableAutoTrack: true,
+      framework: 'nuxt',
+      mode: import.meta.dev ? 'development' : 'production',
+    })
+    pageview({
+      route: route.matched[0]?.path || route.path,
+      path: route.path
+    })
+  })
+  // On navigation to a new page
+  nuxtApp.hooks.hook('page:finish', () => {
+    pageview({
+      route: route.matched[0]?.path || route.path,
+      path: route.path
+    })
+  })
+})
+`,
+    });
+
+    addPlugin({
+      src: template.dst,
+      mode: 'client',
     });
   },
 }) as NuxtModule;
