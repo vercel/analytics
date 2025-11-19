2ef9b2914eae4404507a0567e0e8ed6d252c0048
Sébastien Chopin <atinux@gmail.com>
2025-11-19 12:31:41 +0100

    update example

--- apps/nuxt/app/plugins/analytics.ts
+++ apps/nuxt/app/plugins/analytics.ts
@@ -1,11 +1,15 @@
-// Enable only when disabling the module in nuxt.config.ts
+/**
+ * Enable only when disabling the module in nuxt.config.ts
+ */
 // import { injectAnalytics } from "@vercel/analytics/nuxt"
-// import type { BeforeSendEvent } from "@vercel/analytics/nuxt"
 
 // export default defineNuxtPlugin(() => {
 //   injectAnalytics({
-//     beforeSend(event: BeforeSendEvent) {
-//       console.log('beforeSend')
+//     beforeSend(event) {
+//       console.log('beforeSend', event.url)
+//       if (event.url.includes('/private')) {
+//         return null;
+//       }
 //       return event
 //     }
 //   })
