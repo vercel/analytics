9a406c41f9a5b7575929e1c7157604fde3528169
Sébastien Chopin <atinux@gmail.com>
2025-11-25 08:58:20 +0100

    feat(nuxt): Add support for `injectAnalytics()` and Nuxt module (#178)
    Co-authored-by: Hugo Richard <hugo.richard@vercel.com>

---
+++ apps/nuxt/app/plugins/analytics.ts
@@ -0,0 +1,16 @@
+/**
+ * Uncomment only when disabling the module in nuxt.config.ts
+ */
+// import { injectAnalytics } from "@vercel/analytics/nuxt"
+
+// export default defineNuxtPlugin(() => {
+//   injectAnalytics({
+//     beforeSend(event) {
+//       console.log('beforeSend', event.url)
+//       if (event.url.includes('/private')) {
+//         return null;
+//       }
+//       return event
+//     }
+//   })
+// })
