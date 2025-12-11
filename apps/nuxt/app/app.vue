d544cd7e186bf4782320bd368cd5299214f2875d
Hugo Richard <hugo.richard@vercel.com>
2025-12-11 16:30:14 +0000

    feat(nuxt)!: Add support for `injectAnalytics()` and Nuxt module

---
+++ apps/nuxt/app/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+/**
+ * Uncomment only when disabling the module in nuxt.config.ts
+ * As well as using the deprected <Analytics> component
+ * We recommend using the module or the injectAnalytics function in a Nuxt plugin
+ */
+// import { Analytics, type BeforeSendEvent } from '@vercel/analytics/nuxt/runtime';
+
+// const beforeSend = (event: BeforeSendEvent) => {
+//   console.log('Sending event:', event);
+//   return event;
+// };
+</script>
+
+<template>
+  <!-- <Analytics :before-send="beforeSend" /> -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
