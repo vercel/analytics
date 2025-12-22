c5875a761f9fe3f1ffc882cbf0b13c62f85fb852
Hugo <hugo.richard@vercel.com>
2025-12-22 18:18:29 +0100

    feat(nuxt)!: Add support for `injectAnalytics()` and Nuxt module (#183)

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-11-06',
+  modules: ['@vercel/analytics'],
   devtools: { enabled: true },
 });
