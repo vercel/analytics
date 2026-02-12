218e62c85d6b6438dd5e9fc2d32bb2b673818a74
Scholarly bridge <learnglow021@gmail.com>
2026-02-12 09:38:34 -0800

    Update layout.tsx

--- apps/nextjs/app/layout.tsx
+++ apps/nextjs/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: 'Next.js',
@@ -13,8 +14,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Analytics />
         {children}
+        {/* These two components track your visitors and site speed */}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
